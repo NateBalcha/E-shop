45d3ede95e90c70db64805e4b310129532ba12a9
NateBalcha <natebalcha@gmail.com>
2023-12-10 16:51:29 -0500

    mobile screen adjusted

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,10 +4,14 @@ import { Link } from "react-router-dom";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 import { ShopContext } from "../../Context/ShopContext";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 const Navbar = () => {
   let [menu, setMenu] = useState("shop");
   const { getTotalCartItems } = useContext(ShopContext);
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const handleNavbar = () => setToggleMenu(!toggleMenu);
 
   return (
     <div className="nav">
@@ -82,6 +86,55 @@ const Navbar = () => {
         </Link>
         <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
+
+      {/*  */}
+      <button
+        type="button"
+        className="navbar-toggler-btn"
+        onClick={handleNavbar}
+      >
+        <HiOutlineMenuAlt3
+          className="hamburger-menu"
+          size={35}
+          style={{
+            color: `${toggleMenu ? "#fff" : "#010101"}`,
+          }}
+        />
+      </button>
+
+      <div
+        className={
+          toggleMenu
+            ? "navbar-collapse show-navbar-collapse"
+            : "navbar-collapse"
+        }
+      >
+        <ul className="small-screen-nav-menu">
+          <div className="test">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Shop
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/mens">
+                Mens
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/womens">
+                Womens
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/kids">
+                Kids
+              </Link>
+            </li>
+          </div>
+        </ul>
+      </div>
+      {/*  */}
     </div>
   );
 };
